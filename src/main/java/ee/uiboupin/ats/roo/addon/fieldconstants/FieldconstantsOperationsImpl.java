80e401a417bd11dab8b000e4b9d763642ec9e735
atsuiboupin@gmail.com <atsuiboupin@gmail.com@1c0188ea-5c7f-e398-3560-15c83ae7d572>
2011-10-16 21:41:35 +0000

    Fix: VERSION constant was not updated before release

--- src/main/java/ee/uiboupin/ats/roo/addon/fieldconstants/FieldconstantsOperationsImpl.java
+++ src/main/java/ee/uiboupin/ats/roo/addon/fieldconstants/FieldconstantsOperationsImpl.java
@@ -38,7 +38,7 @@ public class FieldconstantsOperationsImpl implements FieldconstantsOperations {
 
   private static final String GROUP_ID = "ee.uiboupin.ats.roo.addon";
   private static final String ARTIFACT_ID = "fieldconstants";
-  private static final String VERSION = "0.1.0.BUILD-SNAPSHOT";
+  private static final String VERSION = "1.0.0.RC2";
   private static final String PROJECT_NAME = "spring-roo-addon-field-constants";
 
   /**
